6ba7a3fafcaf0cd6c19a69f309a9c7a4a268b2e7
oscar-dif <oscar.c.mild@gmail.com>
2022-06-30 14:46:05 +0200

    Implemented the function read_name with an 'elif action name' branch'

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ def read_phone(C, name):
     rows = cur.fetchall()
     cur.close()
     return rows
+def read_name(C, name):
+    cur = C.cursor()
+    print(f"SELECT name FROM phonelist WHERE name = '{name}';")
+    cur.execute(f"SELECT name FROM phonelist WHERE name = '{name}';")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 def add_phone(C, name, phone):
     cur = C.cursor()
     cur.execute(f"INSERT INTO phonelist VALUES ('{name}', '{phone}');")
@@ -78,6 +85,14 @@ def api_func():
         if len(phone) < 1:
             return "not found"
         return phone[0][0]
+    elif action == 'name':
+        phone = args.get('phone', default="No phone", type=str)
+        if phone == "No phone":
+            return render_template('api_usage.html', action=action)
+        name = read_name(conn, phone)
+        if len(name) < 1:
+            return "not found"
+        return name[0][0]
     else:
         return f"Unknown action: '{action}'"
 
